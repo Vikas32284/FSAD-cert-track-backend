00c3d21d66864feb8678ba00d8850731504b87ce
Vikas32284 <2400032284@kluniversity.in>
2026-04-28 15:55:33 +0530

    fix cors syntax

--- src/main/java/com/institute/skill7/config/CorsConfig.java
+++ src/main/java/com/institute/skill7/config/CorsConfig.java
@@ -10,17 +10,18 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-    .allowedOrigins(
-        "http://localhost:5173",
-        "https://fsad-cert-track-frontend.onrender.com"
-    )
-    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // ✅ IMPORTANT
-    .allowedHeaders("*")
-    .allowCredentials(false);
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://fsad-cert-track-frontend.onrender.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
         };
     }
